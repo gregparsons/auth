80ac719d566f8be76ea82946716f57fca1255415
gp <greg.parsons@inclimesolutions.com>
2023-05-16 20:32:25 -0700

    sqlx

--- push_frontend.sh
+++ push_frontend.sh
@@ -1,1 +1,1 @@
-make sqlx_frontend; git add .;git commit -am "sqlx"; source ~/.bashrc; git push origin dev; ssh swimr205 "cd dev/rust/_trade/trade; export GIT_SSH_COMMAND='ssh -i /home/glp/key/ssh_github_20220411/ssh_github_20220411'; git pull origin dev;make frontend"
+make sqlx_frontend; git add .;git commit -am "sqlx"; source ~/.bashrc; git push origin dev; ssh swimr205 "cd dev/rust/_trade/trade; export GIT_SSH_COMMAND='ssh -i /home/glp/key/ssh_github_20220411/ssh_github_20220411'; git pull origin dev; make frontend"
