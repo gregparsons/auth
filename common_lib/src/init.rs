6bf25e9d050548b603cb6381d66c31513e8e60d3
gp <greg.parsons@inclimesolutions.com>
2023-05-20 11:58:58 -0700

    sqlx

--- common_lib/src/init.rs
+++ common_lib/src/init.rs
@@ -12,7 +12,7 @@ pub fn init(package_name:&str) {
 
     let dot_env_path = match config_location.as_str() {
         "docker" => {
-            "/.env".to_string()
+            ".env".to_string()
         },
         "not_docker" | _ =>{
             // backend/.env
@@ -22,11 +22,13 @@ pub fn init(package_name:&str) {
         }
     };
 
-    println!("[init] dot_env_path: {}", &dot_env_path);
+    let env_file_version = std::env::var("ENV_FILE_VERSION").unwrap_or_else(".env not loaded".to_string());
+
+    println!("[init] dot_env_path: {}; env_file_version: {}", &dot_env_path, &env_file_version);
 
     match dotenvy::from_filename(&dot_env_path) {
-        Ok(_) => tracing::debug!(".env found"),
-        _ => tracing::debug!(".env not found"),
+        Ok(_) => println!("[init] .env found"),
+        _ => println!("[init] .env not found"),
     }
     tracing_subscriber::registry().with(fmt::layer()).with(EnvFilter::from_default_env()).init();
     tracing::debug!("[init] .env file: {}", &dot_env_path);
