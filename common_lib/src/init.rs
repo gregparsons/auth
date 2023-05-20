54c4c1d43066ff04bc30d72bf2c06513fa100798
gp <greg.parsons@inclimesolutions.com>
2023-05-20 13:40:05 -0700

    sqlx

--- common_lib/src/init.rs
+++ common_lib/src/init.rs
@@ -6,10 +6,9 @@ use tracing_subscriber::{fmt, EnvFilter};
 
 pub fn init(package_name:&str) {
 
+    // if docker, load .env from the root directory, otherwise use the cargo workspace directory
     let config_location = std::env::var("CONFIG_LOCATION").unwrap_or_else(|_| "not_docker".to_owned());
-
     println!("[init] config_location: {}", &config_location);
-
     let dot_env_path = match config_location.as_str() {
         "docker" => {
             ".env".to_string()
@@ -21,13 +20,11 @@ pub fn init(package_name:&str) {
             format!("{}/.env", &package_name)
         }
     };
-
-    let env_file_version = std::env::var("ENV_FILE_VERSION").unwrap_or_else(|_| ".env not loaded".to_string());
-
     match dotenvy::from_filename(&dot_env_path) {
         Ok(_) => println!("[init] .env found"),
         _ => println!("[init] .env not found"),
     }
+    let env_file_version = std::env::var("ENV_FILE_VERSION").unwrap_or_else(|_| ".env not loaded".to_string());
     println!("[init] dot_env_path: {}; env_file_version: {}", &dot_env_path, &env_file_version);
 
     tracing_subscriber::registry().with(fmt::layer()).with(EnvFilter::from_default_env()).init();
